f02207fe21aafa9d69a25e64406068e5d77a725f
elisabethmesoy <elisabeth-nm@hotmail.com>
2017-11-15 10:45:56 +0100

    endret i booking

--- controllers/classes.js
+++ controllers/classes.js
@@ -2,7 +2,7 @@ var connection = require('./../config');
 
 module.exports = {
     getClasses: function (req, res) {
-        connection.pool.query('SELECT class_name, level FROM classes WHERE level = ?', [req.params.level], function (err, result){
+        connection.pool.query('SELECT class_name, level, information FROM classes WHERE level = ?', [req.params.level], function (err, result){
             if(err) return res.status(500).json({message: 'Something went wrong.'});
             //console.log(result);
             if(result.length === 0) return res.status(404).json({message: 'Class not found.'});
